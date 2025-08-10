dc683263607f52c510fa4f390c8d390941caefa2
shawon-talukder <shawontalukder.study@gmail.com>
2025-08-11 00:42:30 +0600

    chore: project endpoints, configuration setup

---
+++ tests/support/config/APIConfig.ts
@@ -0,0 +1,28 @@
+class APIEndpoints {
+    static get ACCOUNTS() {
+        return {
+            BASE: "/accounts",
+            BY_ID: (account_id: string) => `/accounts/${account_id}`,
+            BY_CONTACTS: (account_id: string) => this.ACCOUNTS.BY_ID(account_id) + '/contacts',
+            BY_SINGLE_CONTACT: (account_id: string, contact_type: string) => this.ACCOUNTS.BY_CONTACTS(account_id) + `/${contact_type}`,
+            BY_ADDRESSES: (account_id: string) => this.ACCOUNTS.BY_ID(account_id) + '/addresses',
+            BY_SINGLE_ADDRESS: (account_id: string, address_uuid: string) => this.ACCOUNTS.BY_ADDRESSES(account_id) + `/${address_uuid}`,
+        }
+    }
+
+    static get ORDERS() {
+        return {
+            BASE: "/orders",
+            BY_ID: (order_id) => `/orders/${order_id}`
+        }
+    }
+}
+
+const APIConfig = {
+    baseURL: process.env.API_BASE_URL,
+    timeout: process.env.API_TIMEOUT,
+    version: process.env.API_VERSION,
+    endpoints: APIEndpoints
+}
+
+module.exports = { apiConfig: APIConfig, APIEndpoints }
